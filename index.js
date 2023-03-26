a2723dd7aff1d397d5e79419a726e013c156f4e0
lehung2022 <hung.le.programmer@gmail.com>
2023-03-26 18:59:24 +0700

    solving problems

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-console.log(Math.floor(Math.random()*10) + 1);
-console.log(Math.ceil(Math.random()*10) + 1);
-console.log(Math.ceil(Math.random()*10));
+
+// Write function that will return random letter from your name
+
+function generateRandomLetter() {
+    const alphabet = "newman56syntax48Buoyancy20vaguest"
+
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+console.log(generateRandomLetter())
+console.log(generateRandomLetter())
+console.log(generateRandomLetter())
+console.log(generateRandomLetter())
+console.log(generateRandomLetter())
+console.log(generateRandomLetter())
+// The above solution is what I picked up on the Internet. We will see how Dave does it
+// This is my own solution, based on what I have picked up
+
+const myName = "Le Hung. My Hanzi name is 黎興"
+function generateMyName() {  
+    
+    return myName[Math.floor(Math.random() * myName.length)]
+}
+
+console.log(generateMyName());
+
+// Dave's solution is here
+const hisName = "Le Hung/黎興"; 
+console.log(typeof hisName);
+console.log(hisName.charAt(Math.floor(Math.random() * 6)));
--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-console.log(Math.floor(Math.random()*10) + 1);
-console.log(Math.ceil(Math.random()*10) + 1);
-console.log(Math.ceil(Math.random()*10));
+
+// Write function that will return random letter from your name
+
+// This is my own solution, based on what I have picked up
+
+const myName = "黎興獨行道烈火拳逆鱗拳烈火真拳"
+function generateMyName() {  
+    
+    return myName[Math.floor(Math.random() * myName.length)]
+}
+
+console.log(generateMyName());
+
+// Dave's solution is here
+const hisName = "Le Hung"; 
+console.log(typeof hisName);
+console.log(hisName.charAt(Math.floor(Math.random() * 6)));
+console.log(hisName.charAt(Math.floor(Math.random() * hisName.length)));
