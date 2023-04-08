dbc5a148fde4d98087b74e4f5c6de459cd22f450
lehung2022 <hung.le.programmer@gmail.com>
2023-04-08 12:15:10 +0700

    changed the compiler

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-// User Input: Play game
-let playGame = confirm("shall we play the game");
-// Check the value and respond
-if (playGame) {
-
-} else {
-    alert("Better luck next time, buddy");
+// Intro 
+// While Loops
+let myNumber = 0;
+while (myNumber < 51) {
+    console.log(myNumber);
+    myNumber = myNumber + 1;
 }
-// Player chooses rock, paper, or scissors
-let playerChoice = prompt("please enter rock, paper, or scissors");
-// Handle the response
-if (playerChoice) {
-    let playerOne = playerChoice.trim().toLowerCase();
-    if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
 
-
-    } else {
-        alert("you did not enter anything, my man");
-    }
-} else {
-    alert("I guess you changed your mind")
-}
-// Computer player makes a choice
-let computerChoice = Math.floor(Math.random() * 3 + 1);
-let computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
-// Rock-Paper-Scissors logic tree
-let result =
-    playerOne === computer
-        ? "tie game"
-        : playerOne === "rock" && computer === "paper"
-            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins`
-            : playerOne === "paper" && computer === "scissors"
-                ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins`
-                : playerOne === "scissors" && computer === "rock"
-                    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins`
-                    : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins`
-alert(result);
-// Display winner! Ask to play again
-let playAgain = confirm("Play Again????");
-playAgain ? location.reload() : alert("Ok, thank you for playing");
-// Walk-thru of code logic
-// Ready to test the code
-// An area for improvement
-
-// Congrats on completing your first interactive game!
+// Do not create an endless loop (but don't feel bad if you do..it happens)
+// Do While Loops
+// The difference of a Do While Loop
+// For Loops
+// Initialize the counter variable
+// Conditional statement
+// Increment the counter
+// Using the length property of a variable in a for loop
+// While (true)
+// Break Statements
+// Continue Statements
--- js/main.js
+++ js/main.js
@@ -1,37 +1,26 @@
-//#1 User Input: Play game
-let playGame = confirm("shall we play the game");
-//#2 Check the value and respond
-if (playGame) {
-  //#3 Player chooses rock, paper, or scissors
-  let playerChoice = prompt("please enter rock, paper, or scissors");
-  //#4 Handle the response
-  if (playerChoice) {
-    let playerOne = playerChoice.trim().toLowerCase();
-    if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
-      //#5 Computer player makes a choice
-      let computerChoice = Math.floor(Math.random() * 3 + 1);
-      let computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
-      //#6 Rock-Paper-Scissors logic tree
-      let result =
-        playerOne === computer
-          ? "tie game"
-          : playerOne === "rock" && computer === "paper"
-            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins`
-            : playerOne === "paper" && computer === "scissors"
-              ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins`
-              : playerOne === "scissors" && computer === "rock"
-                ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins`
-                : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins`
-      alert(result);
-      //#7 Display winner! Ask to play again
-      let playAgain = confirm("Play Again????");
-      playAgain ? location.reload() : alert("Ok, thank you for playing");
-    } else {
-      alert("you did not enter anything, my man");
-    }
-  } else {
-    alert("I guess you changed your mind")
-  }
-} else {
-  alert("Better luck next time, buddy");
-}
+// Intro 
+// Loops and Iteration will be the main topic of today's lesson
+// While Loops
+
+// Do not create an endless loop (but don't feel bad if you do..it happens)
+
+// Do While Loops
+
+// The difference of a Do While Loop
+
+// For Loops
+
+// Initialize the counter variable
+
+// Conditional statement
+
+// Increment the counter
+
+// Using the length property of a variable in a for loop
+
+// While (true)
+
+// Break Statements
+
+// Continue Statements
+
