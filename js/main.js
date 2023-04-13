1e5c98125826fae2f5a284ee323c45cdcbf3f75f
lehung2022 <hung.le.programmer@gmail.com>
2023-04-13 11:08:42 +0700

    javascript var, let, const and scope

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-const toProperCase = (name) => {
-	return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+var x = 5;
+// let y = 6; 
+// const z = 8;
+// local scope. There are ... types of local scopes
+// first type
+
+// second type
+function add() {
+	let z = 10;
+	console.log(x);
+	{
+		let y = 9;
+		console.log(y);
+	}
 }
-console.log(toProperCase("December"));
+add();
