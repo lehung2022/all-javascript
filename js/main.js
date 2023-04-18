b6575ad4f22406f54ec6174a4d66da4a351c1e31
lehung2022 <hung.le.programmer@gmail.com>
2023-04-18 12:59:16 +0700

    rock, paper, scissors

--- js/main.js
+++ js/main.js
@@ -1,8 +1,81 @@
-// Arrays
-let myWeapons = ["nunchaku", "knives", "bo staff"];
+const options = document.querySelectorAll(".options");
+let pScore = 0;
+let cScore = 0;
 
-// shift will remove an element at the beginning of the array
-const extra = myWeapons.push("intervention bolt action rifle"); 
-console.log(myWeapons);
-// reference to any items in the array 
-console.log(myWeapons[3]);
+options.forEach((option) => {
+  option.addEventListener("click", function () {
+    const pInput = this.value;
+
+    const cOptions = ["Rock", "Paper", "Scissors"];
+    const cInput = cOptions[Math.floor(Math.random() * 3)];
+
+    updateMoves(pInput, cInput);
+    compareInputs(pInput, cInput);
+    updateScore();
+    if (checkWinner()) {
+      pScore = cScore = 0;
+      updateScore();
+    }
+  });
+});
+
+function updateMoves(pInput, cInput) {
+  document.getElementById("p-move").src = `./images/${pInput}.svg`;
+  document.getElementById("c-move").src = `./images/${cInput}.svg`;
+}
+
+
+function compareInputs(pInput, cInput) {
+  const currentMatch = `${pInput} vs ${cInput}`;
+  if (pInput === cInput) {
+    alert(`${currentMatch} is a Tie`);
+    return;
+  }
+
+  if (pInput === "Rock") {
+    if (cInput === "Scissors") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+  //Check for Paper
+  else if (pInput === "Paper") {
+    if (cInput === "Rock") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+  //Check for Scissors
+  else {
+    if (cInput === "Paper") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+}
+
+function updateScore() {
+  document.getElementById("p-score").textContent = pScore;
+  document.getElementById("c-score").textContent = cScore;
+}
+
+function checkWinner() {
+  if (pScore === 5 || cScore === 5) {
+    const winner =
+      pScore === 5
+        ? "You win the game! Congratulations!"
+        : "Computer wins the game! Try again next time!";
+    alert(winner);
+    return true;
+  }
+  return false;
+}
