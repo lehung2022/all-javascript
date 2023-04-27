00d0a15b5800eeab4f09ef588b132a3e70daa029
lehung2022 <hung.le.programmer@gmail.com>
2023-04-27 16:59:32 +0700

    chapter 17

--- js/main.js
+++ js/main.js
@@ -1,81 +1,15 @@
-const options = document.querySelectorAll(".options");
-let pScore = 0;
-let cScore = 0;
-
-options.forEach((option) => {
-  option.addEventListener("click", function () {
-    const pInput = this.value;
-
-    const cOptions = ["Rock", "Paper", "Scissors"];
-    const cInput = cOptions[Math.floor(Math.random() * 3)];
-
-    updateMoves(pInput, cInput);
-    compareInputs(pInput, cInput);
-    updateScore();
-    if (checkWinner()) {
-      pScore = cScore = 0;
-      updateScore();
-    }
-  });
-});
-
-function updateMoves(pInput, cInput) {
-  document.getElementById("p-move").src = `./images/${pInput}.svg`;
-  document.getElementById("c-move").src = `./images/${cInput}.svg`;
+const nikoKatas = {
+    name: "二虎流",
+    firstForm: "操流型",
+    secondForm: "金剛型",
+    thirdForm: "火天型",
+    fourthForm: "水天型"
 }
 
+const {firstForm: myForm, name: myName} = nikoKatas;
+console.log(myForm);
 
-function compareInputs(pInput, cInput) {
-  const currentMatch = `${pInput} vs ${cInput}`;
-  if (pInput === cInput) {
-    alert(`${currentMatch} is a Tie`);
-    return;
-  }
-
-  if (pInput === "Rock") {
-    if (cInput === "Scissors") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-  //Check for Paper
-  else if (pInput === "Paper") {
-    if (cInput === "Rock") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-  //Check for Scissors
-  else {
-    if (cInput === "Paper") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
+function fight({firstForm}) {
+    return `${firstForm} fight`;
 }
-
-function updateScore() {
-  document.getElementById("p-score").textContent = pScore;
-  document.getElementById("c-score").textContent = cScore;
-}
-
-function checkWinner() {
-  if (pScore === 5 || cScore === 5) {
-    const winner =
-      pScore === 5
-        ? "You win the game! Congratulations!"
-        : "Computer wins the game! Try again next time!";
-    alert(winner);
-    return true;
-  }
-  return false;
-}
+console.log(fight(nikoKatas));
