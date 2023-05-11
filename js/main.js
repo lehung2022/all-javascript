e50e09b2b554ee7fd7b9be21fd8c31f532b96c61
lehung2022 <hung.le.programmer@gmail.com>
2023-05-11 15:21:39 +0700

    chapter 23 has arrived

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-
-document.addEventListener("readystatechange", (event) => {
-    if (event.target.readyState === "complete") {
-        console.log("readyState: complete");
-        initApp();
-    }
-})
-// (34:50) Listening for a HTML Form submit event
-// (36:20) HTML Form default behavior
-// (37:00) Prevent default behavior with event.preventDefault()
-const initApp = () => {
-    const view3 = document.querySelector("#view3");
-    const myForm = view3.querySelector("#myForm");
-    myForm.addEventListener("submit",(event) => {
-        event.preventDefault();
-        console.log("submit event");
-    })
-};
+// This chapter will be about Local and Session Storage
