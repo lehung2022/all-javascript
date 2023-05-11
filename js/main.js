129a6a28437d566d09fabcd9a36fedf2e79587a6
lehung2022 <hung.le.programmer@gmail.com>
2023-05-11 15:15:16 +0700

    chapter 22 is done

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
-// (3:30) Syntax: addEventListener(event, function, useCapture)
 
-// (4:50) Using a function name in the event listener
-
-// (5:55) Removing a function with a name: removeEventListener()
-
-// (6:50) Using an anonymous function in the event listener
-
-// (8:50) Listening for the readystatechange event
-
-// (10:30) Constructing an initApp() function
-
-// (13:30) Event Bubbling
-
-// (16:00) Event Capturing
-
-// (17:00) Event Propagation and stopPropagation()
-
-// (21:45) event.target usage
-
-// (25:05) classList, add, remove, and toggle
-
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+})
 // (34:50) Listening for a HTML Form submit event
-
 // (36:20) HTML Form default behavior
-
 // (37:00) Prevent default behavior with event.preventDefault()
+const initApp = () => {
+    const view3 = document.querySelector("#view3");
+    const myForm = view3.querySelector("#myForm");
+    myForm.addEventListener("submit",(event) => {
+        event.preventDefault();
+        console.log("submit event");
+    })
+};
