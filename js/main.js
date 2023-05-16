9983419ddd4de60e4e854cd018648a4f6401bc89
lehung2022 <hung.le.programmer@gmail.com>
2023-05-16 13:14:36 +0700

    temporary skip

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// This chapter will be about Javascript Module
-import playGuitar from './guitar';
-import Guitar from '../components/music';
-
-console.log(Guitar());
-console.log(playGuitar());
---
+++ static/main.js
@@ -0,0 +1,6 @@
+// This chapter will be about Higher Order functions
+import posts from './posts';
+
+posts.forEach((post) => {
+    console.log(post);
+})
